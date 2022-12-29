bdfd9457ccd672d8625737a7e80fbd8af29ec994
FeridQarayev <tu7he6n9s@code.edu.az>
2022-12-29 07:42:06 -0800

    add filtered api react

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import "./style.css";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [inpVal, setInpVal] = useState("");
+  useEffect(()=>{
+    axios.get("https://randomuser.me/api/?results=100").then((res)=>setUsers(res.data.results));
+  },[])
+  return (
+    <div className='body'>
+      <div className='container'>
+        <div className='container__up'>
+          <h4>Live User Filter</h4>
+          <p>Search by user name and/or location</p>
+          <input type='text' placeholder='Search' onChange={(e)=>setInpVal(e.target.value)} />
+        </div>
+        <div className='list'>
+          <ul>
+            {
+              users.filter((filltered)=>{
+                let name = `${filltered?.name?.title} ${filltered?.name?.first} ${filltered?.name?.last}`;
+                if(inpVal=='') return filltered
+                if (name.toLowerCase().includes(inpVal.toLowerCase())) {
+                  return filltered
+                }
+              })
+              .map((user,index)=>{
+                return (
+                  <li key={index}>
+                    <img src={user?.picture?.thumbnail} alt={user?.name} />
+                    <div className='user__info'>
+                      <h4>{user?.name?.title} {user?.name?.first} {user?.name?.last}</h4>
+                      <span>{user.location.city} {user.location.state} {user.location.country}</span>
+                    </div>
+                  </li>
+                  )
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
